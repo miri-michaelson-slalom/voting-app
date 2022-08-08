a0ddcfb36302c1d697fd3e9d7f9fb1f31d29e753
Miri Michaelson <miri.michaelson@slalom.com>
2022-08-08 10:44:41 -0400

    export images

---
+++ client/src/public/images/index.js
@@ -0,0 +1,19 @@
+import DefaultPlant1 from './plants/default/plant-1-default.svg';
+import DefaultPlant2 from './plants/default/plant-2-default.svg';
+import DefaultPlant3 from './plants/default/plant-3-default.svg';
+import DefaultPlant4 from './plants/default/plant-4-default.svg';
+import SelectedPlant1 from './plants/selected/plant-1-selected.svg';
+import SelectedPlant2 from './plants/selected/plant-2-selected.svg';
+import SelectedPlant3 from './plants/selected/plant-3-selected.svg';
+import SelectedPlant4 from './plants/selected/plant-4-selected.svg';
+
+export {
+  DefaultPlant1,
+  DefaultPlant2,
+  DefaultPlant3,
+  DefaultPlant4,
+  SelectedPlant1,
+  SelectedPlant2,
+  SelectedPlant3,
+  SelectedPlant4
+};
